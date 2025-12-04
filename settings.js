c153d15cf1793a1ca92bd17f52e452a454b40055
voltara50 <akoubosena@gmail.com>
2025-12-04 05:03:44 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "Ice~SEMtcD", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "22890902462", // Put Owner Number Here 
+  SESSION_ID: "", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2238893782", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
