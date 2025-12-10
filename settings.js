ac3b5268dae2d6949d40b74a819717a73429223d
voltara50 <akoubosena@gmail.com>
2025-12-10 01:27:35 +0000

    Update settings.js for Voltara-md

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "Ice~wgkDyU", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "22890902462", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~2uQG92",
+  OWNER_NUMBER: "22890902462", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
